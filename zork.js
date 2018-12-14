e216b51b7561d426b2714fa43c7a86feeee39a7a
scottlarkin <scolark27@gmail.com>
2018-12-14 14:51:55 +0000

    Update zork.js

--- zork.js
+++ zork.js
@@ -1,52 +1,43 @@
 const prompt = require('prompts');
-async function name(){
-   return await prompt ({
-        type: 'text',
-        name: 'value',
-        message: 'What is your name?',
-    });
-}
-name().then(function(response) {
-   // How do I make it wait until I press enter until the next line of code runs? console.log(`Hello ${response.value}, welcome to the game of idk yet, press enter to begin.`)
-})
 
-function gamestart(){
-    console.log("You wake up in a daze, you do not know where you are or how you got there, upon looking around you notice the entrance to a cave and a path that leads to a forest, the cave has a torch but no means of lighting it except for a wet match in your pocket, the path that leads to the forest seems ominous but it could be your way out, what do you choose?")
-    
-}
-setTimeout(gamestart, 5000);
-
-/*function decide1() {
-const prompts = require('prompts');
-let response = prompts({
+// abstract the prompt a bit to simplify the interface
+const askQuestion = async message => (await prompt({
     type: 'text',
     name: 'value',
-    message: 'Do you choose the forest or the cave? (simply type forest or cave to decide)',
-});
+    message
+})).value;
+
+// this asks a question, and if the response is invalid, asks the question again
+const multipleChoiceQuestion = async (question, possibleAnswers) => {
+
+    // ask the question and store the answer here
+    const answer = await askQuestion(question);
+
+    // check the answer matches one of the possible answers
+    if (!possibleAnswers.includes(answer)){
+
+        // if the answer doesnt match one of the allowed answers, ask again
+        console.log(`Invalid answer: ${answer}`);
+        return multipleChoiceQuestion(question, possibleAnswers);
+    } 
+
+    return answer;
 }
-decide1()
-This runs before it even asks for my name, how do I make it wait until it reaches this line before it executes immediately?
-*/
 
+const gamestart = async () => {
 
+    // ask the first question and await the response, storign the answer in the playerName constant
+    const playerName = await askQuestion("What is your name");
+    
+    console.log(`Hi ${playerName}`);
 
+    console.log("You wake up in a daze, you do not know where you are or how you got there, upon looking around you notice the entrance to a cave and a path that leads to a forest, the cave has a torch but no means of lighting it except for a wet match in your pocket, the path that leads to the forest seems ominous but it could be your way out, what do you choose?")
 
+    // ask the second question and await the response, storing the answer in the answer1 constant
+    const answer1 = await multipleChoiceQuestion("Do you choose the forest or the cave? (simply type forest or cave to decide)", ["cave", "forest"]);
 
-
-
-/*
-//Currently have this commented out so I can test indiivudal lines before allowing it all to execute to isolate errors.
-if (decision1 === "forest".toLowerCase()) {
-    console.log("You venture into the forest, not sure where to go next, along the path you encounter a bush filled with wild berries, your are terribly hungry");
-var berries = prompt("Do you wish to eat the berries? yes or no");
-if (berries === "yes") {
-    console.log("You eat the berries and venture on, you notice your stomach is kind of queasy but you feel content so you shrug it off");
+    console.log(`You chose the ${answer1}`);
 }
-    else {
-        console.log("You venture on into the path of the unkown forest, your stomach growls in agonizing hunger, you hope you find some food soon.");
-    }
-}
-else {
-    console.log("You walk up to the torch and grab your wet match, you attempt to strike it on a rock to ignite it but it breaks in half, you end up venturing into the cave in complete darkness.")
-}
-*/
+
+// start the game
+gamestart();
